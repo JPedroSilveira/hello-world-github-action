5efb0c8ab58b8201e7219ebcc43ab26903917856
João Pedro <jpedross1999@gmail.com>
2021-12-12 18:02:55 -0300

    feat: setup hello world action

---
+++ .github/actions/hello-world/index.js
@@ -0,0 +1,19 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const name = core.getInput("who-to-greet");
+
+  if (name === "error") {
+    throw new Error('Name "error" is invalid!');
+  }
+
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toISOString());
+
+  console.log(JSON.stringify(github, null, "\t"));
+} catch (error) {
+  core.setFailed(error.message);
+}
